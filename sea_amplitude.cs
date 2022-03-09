0b88dd3b2a3d8287ccc6cd8bb63a25146004d394
bdeniz1997 <94968640+bdeniz1997@users.noreply.github.com>
2022-03-09 13:26:14 +0300

    Create sea_amplitude.cs

---
+++ sea_amplitude.cs
@@ -0,0 +1,18 @@
+/*
+The idea behind this is to get the world height with the biggest amplitude and scale,
+and use that Y value to change the sea amplitude throughout the world.
+depending on if its deep or shallow.
+lets say the y( heigtValue) is between (0~-500) wave amplitude is going to be between (0.31~ 7 or sth)
+an inverse lerp.
+float amp = inverseLerp(0,-500,heightVal);
+and a animation curve.
+amp = 0 -> finalAmp = 0.31
+amp = 1 -> finalAmp = 7
+
+and so on.
+
+we'll multiply the current amplitude with that value from our static class. and the buoyancy is done.
+on shader side, i want to have the texture and use it in shader. that'll make it good.
+or maybe ill texture grayscale pick on scripts as well. who knows :D
+
+*/
