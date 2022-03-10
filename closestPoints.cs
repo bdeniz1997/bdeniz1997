bc5863204d899bb3adcb215c681ab94fc7c70be6
bdeniz1997 <94968640+bdeniz1997@users.noreply.github.com>
2022-03-11 00:04:41 +0300

    Update closestPoints.cs

--- closestPoints.cs
+++ closestPoints.cs
@@ -16,51 +16,25 @@ public class closestPoints : MonoBehaviour
     public float forceFactor;
     computeit computeitscript;
     UnityEngine.Rendering.AsyncGPUReadbackRequest request;
-    public Vector4 waveA,waveB,waveC,waveD,waveE,waveF,waveG;
     //int size=0;
 
     private void Awake() {
         computeShader = (ComputeShader)Instantiate(Resources.Load ("closestOne"));
         computeitscript= FindObjectOfType<computeit>();
-        // waveA= computeitscript.waveA;
-        // waveB= computeitscript.waveB;
-        // waveC= computeitscript.waveC;
-        // waveD= computeitscript.waveD;
-        // waveE= computeitscript.waveE;
-        // waveF= computeitscript.waveF;
-        // waveG= computeitscript.waveG;
         
     }
     void Start()
     {
         int kernel=computeShader.FindKernel("CSMain");
-        //Computeit= transform.parent.GetComponent<computeit>();
-        //mesh=computeitscript.PublicMesh;
-        waveA= waveManager.Waves[0];
-        waveB= waveManager.Waves[1];
-        waveC= waveManager.Waves[2];
-        waveD= waveManager.Waves[3];
-        waveE= waveManager.Waves[4];
-        waveF= waveManager.Waves[5];
-        waveG= waveManager.Waves[6];
         
         positions= new Vector3[transform.childCount]; 
         for(int i=0;i<transform.childCount;i++){ // initialize array.
             positions[i] = new Vector3(transform.GetChild(i).position.x,0,transform.GetChild(i).position.z);
         }
-
-         
-        
-        //mesh= water.GetComponent<MeshFilter>().mesh.vertices;
-        
-        //mesh= new Vector3[size];
-        //mesh= computeitscript.PublicMesh;
-        //mesh=water.gameObject.GetComponent<MeshFilter>().mesh.vertices;
         
         buffer = new ComputeBuffer(transform.childCount, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
        meshBuffer = new ComputeBuffer(mesh.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
         
-        //mesh=meshObj.gameObject.GetComponent<MeshFilter>().mesh.vertices;
         computeShader.SetBuffer(kernel,"positions", buffer);
         computeShader.SetBuffer(kernel,"MeshData", meshBuffer);
         request= UnityEngine.Rendering.AsyncGPUReadback.Request(buffer);
@@ -70,20 +44,10 @@ public class closestPoints : MonoBehaviour
     void Update()
     {
         
-            //mesh=water.GetComponent<MeshFilter>().mesh.vertices;
-
-            //mesh= computeitscript.PublicMesh;
-            //mesh=meshObj.gameObject.GetComponent<MeshFilter>().mesh.vertices;
             meshBuffer.SetData( mesh);
 
             computeShader.SetFloat("_Time", Time.timeSinceLevelLoad);
             computeShader.SetVector("_WaveA",waveA);
-            computeShader.SetVector("_WaveB",waveB);
-            computeShader.SetVector("_WaveC",waveC);
-            computeShader.SetVector("_WaveD",waveD);
-            computeShader.SetVector("_WaveE",waveE);
-            computeShader.SetVector("_WaveF",waveF);
-            computeShader.SetVector("_WaveG",waveG);
 
             for(int i=0;i<transform.childCount;i++){ // initialize array.
                 positions[i] = transform.GetChild(i).position;
@@ -109,38 +73,11 @@ public class closestPoints : MonoBehaviour
             //Request AsyncReadback again
             request= UnityEngine.Rendering.AsyncGPUReadback.Request(buffer);
         }
-
-            // for(int i=0;i<transform.childCount;i++){
-            //     Debug.DrawRay((ClosestPoint[i]),Vector3.up ,Color.red,0.2f);
-            // }
     }
 
     private void OnDestroy() {
         buffer.Release();
         meshBuffer.Release();
-    }
-
-    private void FixedUpdate() {
-        // if(ClosestPoint!=null){
-        //     for(int i=0;i<transform.childCount;i++){
-        //         Debug.DrawRay(positions[i],Vector3.up,Color.green,2f);
-        //         Debug.DrawRay(ClosestPoint[i],Vector3.up,Color.blue,2f);
-        //     }
-        // }
-        
-
-        // if(ClosestPoint!=null){
-        //     for(int i=0;i<transform.childCount;i++){
-        //         if(ClosestPoint[i].y> positions[i].y){
-        //             Vector3 force = Vector3.up* (ClosestPoint[i].y - positions[i].y);
-        //             if(force==Vector3.zero) force += new Vector3(0,0.1f,0);
-                    
-        //             rb.AddForceAtPosition(force*forceFactor,positions[i],ForceMode.Impulse);
-        //             Debug.DrawRay(ClosestPoint[i],Vector3.up,Color.blue,2f);
-        //         }
-        //     }
-        // }
-    }
-    
+    }   
 
 }
