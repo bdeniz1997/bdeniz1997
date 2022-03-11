aabc53d01d65aa37a259306197ee50da815b80bf
bdeniz1997 <94968640+bdeniz1997@users.noreply.github.com>
2022-03-11 16:40:29 +0300

    Update closestPoints.cs

--- closestPoints.cs
+++ closestPoints.cs
@@ -6,46 +6,38 @@ public class closestPoints : MonoBehaviour
 {
     // Start is called before the first frame update
     ComputeShader computeShader;
-    ComputeBuffer buffer;
     ComputeBuffer meshBuffer;
-    Vector3[] positions;
     Vector3[] mesh;
-    Vector3[] ClosestPoint;
-    Unity.Collections.NativeArray<Vector3> array;
+    Vector3[,] vertexPoints;
+    int[,] index;
+    
     public Rigidbody rb;
     public float forceFactor;
-    computeit computeitscript;
-    UnityEngine.Rendering.AsyncGPUReadbackRequest request;
     //int size=0;
 
     private void Awake() {
-        computeShader = (ComputeShader)Instantiate(Resources.Load ("closestOne"));
-        computeitscript= FindObjectOfType<computeit>();
-        
+        computeShader = (ComputeShader)Instantiate(Resources.Load ("closestOne"));        
     }
     void Start()
     {
         int kernel=computeShader.FindKernel("CSMain");
         
-        positions= new Vector3[transform.childCount]; 
-        for(int i=0;i<transform.childCount;i++){ // initialize array.
-            positions[i] = new Vector3(transform.GetChild(i).position.x,0,transform.GetChild(i).position.z);
-        }
-        
-        buffer = new ComputeBuffer(transform.childCount, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
+        //positions:
+   
        meshBuffer = new ComputeBuffer(mesh.Length, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector3)));
         
-        computeShader.SetBuffer(kernel,"positions", buffer);
         computeShader.SetBuffer(kernel,"MeshData", meshBuffer);
-        request= UnityEngine.Rendering.AsyncGPUReadback.Request(buffer);
+        
+        meshBuffer.SetData(mesh);
+        
+        computeShader.Dispatch(0,transform.childCount,1,1);
+        
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-            meshBuffer.SetData( mesh);
-
             computeShader.SetFloat("_Time", Time.timeSinceLevelLoad);
             computeShader.SetVector("_WaveA",waveA);
 
@@ -53,7 +45,6 @@ public class closestPoints : MonoBehaviour
                 positions[i] = transform.GetChild(i).position;
             }
 
-            buffer.SetData(positions);
             computeShader.Dispatch(0,transform.childCount,1,1);
             ClosestPoint =  new Vector3[transform.childCount];
             //buffer.GetData(ClosestPoint);
