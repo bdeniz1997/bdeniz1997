6106489016e018b509e3a3a2e069b94c25da688f
bdeniz1997 <94968640+bdeniz1997@users.noreply.github.com>
2022-03-21 12:13:12 +0300

    Create ideas.cs

---
+++ ideas.cs
@@ -0,0 +1,26 @@
+/*
+
+----1---
+
+The idea of doing things differently!
+
+this one is about the collision and the mesh deformation.
+
+i'll put a script on every collider first.
+
+onCannonBall script, i'll make whatever it touches, add a rigidbody to it.
+set it to usegravity false, this way its going to move.
+
+and i'll try to match that with the collider.
+
+-then i'll delete it -
+
+
+----2---
+make thousands of little colliders for every 10 vertex or so.
+and use our implemented technique to move them around. lets see if we can do that.
+this looks promising but i can't say the same about the performance. lolz.
+whatever lets go.
+
+
+*/
